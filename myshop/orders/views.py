b86bf8695718a06cd24fdcb63f54cf6b656d6865
Alorian <slowpok19450509@gmail.com>
2024-01-15 15:14:11 +0100

    Async Mail Sending Added (Celery & RabbitMQ)

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 
 def order_create(request: HttpRequest):
@@ -17,6 +18,8 @@ def order_create(request: HttpRequest):
                                          price=item['price'],
                                          quantity=item['quantity'])
                 cart.clear()
+
+                order_created.delay(order.id)
                 return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
